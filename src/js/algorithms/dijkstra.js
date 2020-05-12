816c3b2176711383fe49427ae08707102d6410d0
shreeviknesh <luckyboy1998@gmail.com>
2020-05-12 13:48:08 +0530

    Implemented Dijkstra

---
+++ src/js/algorithms/dijkstra.js
@@ -0,0 +1,49 @@
+// helper function to insert node into priority queue for dijkstra
+async function DijkstraPQinsert(PriorityQueue, node) {
+    for (let i = 0; i < PriorityQueue.length; i++) {
+        if (node.gScore < PriorityQueue[i].gScore) {
+            PriorityQueue.splice(i, 0, node);
+            return;
+        }
+    }
+    PriorityQueue.push(node);
+}
+
+async function dijkstra() {
+    let PriorityQueue = [board.start];
+    board.start.seen = true;
+
+    for (let i = 0; i < this.rows; i++) {
+        for (let j = 0; j < this.cols; j++) {
+            this.grid[i][j].gScore = Infinity;
+        }
+    }
+    board.start.gScore = 0;
+
+    while (PriorityQueue.length > 0 && !interrupt) {
+        let current = PriorityQueue.splice(0, 1)[0];
+        current.show(visitedColor);
+
+        if (current == board.end) {
+            await drawPath();
+            return;
+        }
+
+        for (let pos of board.getNeighbors(current)) {
+            let node = board.grid[pos[0]][pos[1]];
+
+            if (interrupt) {
+                return;
+            }
+
+            if (node.seen == false && node.wall == false) {
+                node.show(discoveredColor);
+                node.seen = true;
+                node.parent = current;
+                node.gScore = current.gScore;
+                DijkstraPQinsert(PriorityQueue, node);
+                await sleep(1000 / fps);
+            }
+        }
+    }
+}
